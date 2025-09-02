1b763ff747118e5877ba427c2ec7bf381163fe60
jessica-ewald <33487514+jessica-ewald@users.noreply.github.com>
2025-09-02 15:38:13 +0100

    Update install_env.sh

--- 00.first_analysis_module/install_env.sh
+++ 00.first_analysis_module/install_env.sh
@@ -7,3 +7,6 @@ uv pip install -r requirements.txt
 python -m ipykernel install --user --name=mod-00-env --display-name "Python (mod-00-env)"
 
 echo "To activate later: source mod-00-env/bin/activate"
+echo "To install a package: uv pip install pkg-name"
+echo "To update requirements.txt after installing packages: uv pip freeze > requirements.txt"
+
